a711d2e066d13a746408fd1e5e899e6e206a15fe
Carlos Garcia <neorazorx@gmail.com>
2012-06-03 20:57:45 +0200

    - Añadidas estádísticas de visitas.

--- model/ke_log.php
+++ model/ke_log.php
@@ -3,7 +3,7 @@
 require_once 'core/ke_cache.php';
 require_once 'core/ke_tools.php';
 
-class ke_log_line
+class ke_log_line extends ke_tools
 {
    public $date;
    public $ip;
@@ -29,15 +29,23 @@ class ke_log_line
       }
       $this->info = $txt;
    }
+   
+   public function timesince()
+   {
+      return $this->var2timesince($this->date);
+   }
 }
 
 class ke_log extends ke_cache
 {
    private $history;
+   private $stats;
    
    public function __construct()
    {
       $this->history = $this->get_array('log_history');
+      $this->stats = $this->get_array('stats');
+      $this->url2stats();
    }
    
    public function clean()
@@ -45,6 +53,10 @@ class ke_log extends ke_cache
       unset($this->history);
       $this->history = array();
       $this->delete('log_history');
+      
+      unset($this->stats);
+      $this->stats = array();
+      $this->delete('stats');
    }
    
    public function new_line($txt='')
@@ -53,9 +65,41 @@ class ke_log extends ke_cache
       $this->set('log_history', $this->history);
    }
    
+   private function url2stats()
+   {
+      $encontrada = FALSE;
+      foreach($this->stats as &$s)
+      {
+         if($s[0] == $_SERVER['REQUEST_URI'])
+         {
+            $s[1] += 1;
+            $encontrada = TRUE;
+         }
+      }
+      if( !$encontrada )
+         $this->stats[] = array($_SERVER['REQUEST_URI'], 1);
+      $this->set('stats', $this->stats);
+   }
+
    public function get_history()
    {
       return array_reverse($this->history);
+   }
+   
+   public function get_stats()
+   {
+      $stts = array();
+      while(count($stts) < count($this->stats))
+      {
+         $element = array('/', 0);
+         foreach($this->stats as $s)
+         {
+            if($s[1] > $element[1] AND !in_array($s, $stts))
+               $element = $s;
+         }
+         $stts[] = $element;
+      }
+      return $stts;
    }
 }
 
